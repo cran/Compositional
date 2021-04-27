257c6d71fdd3fa65f5404b8ba4dba757a508b81c
Michail Tsagris <mtsagris@uoc.gr>
2021-04-27 14:40:02 +0000

    version 4.6

--- R/compppr.tune.R
+++ R/compppr.tune.R
@@ -1,18 +1,18 @@
-compppr.tune <- function(y, x, nfolds = 10, folds = NULL, seed = FALSE, nterms = 1:10, type = "alr", 
-  yb = NULL, B = 1000 ) {
-  
-  runtime <- proc.time() 
+compppr.tune <- function(y, x, nfolds = 10, folds = NULL, seed = FALSE, nterms = 1:10, type = "alr", yb = NULL) {
+
+  runtime <- proc.time()
   if ( is.null(yb) )  {
     if ( type == "alr" ) {
       yb <- alr(y)
     } else   yb <- alfa(y, 0, h = TRUE)$aff
   }
-  
-  D <- dim(y)[2]
+
   n <- dim(y)[1]
+  D <- dim(y)[2]
   nt <- nterms
   ina <- 1:n
-  if ( is.null(folds) )  folds <- Compositional::makefolds(ina, nfolds = nfolds, 
+  if ( !is.matrix(x) )  x <- as.matrix(x)
+  if ( is.null(folds) )  folds <- Compositional::makefolds(ina, nfolds = nfolds,
                                   stratified = FALSE, seed = seed)
   nfolds <- length(folds)
 
@@ -21,7 +21,7 @@ compppr.tune <- function(y, x, nfolds = 10, folds = NULL, seed = FALSE, nterms =
   kl <- matrix( NA, nrow = nfolds, ncol = length(nt) )
   rownames(kl) <- paste("Fold", 1:nfolds, sep = " ")
   colnames(kl) <- paste("nterms=", nt, sep = " ")
-  
+
   for (i in 1:nfolds) {
     nu <- folds[[ i ]]
     ytrain <- y[-nu, ]
@@ -36,32 +36,10 @@ compppr.tune <- function(y, x, nfolds = 10, folds = NULL, seed = FALSE, nterms =
       kl[i, j] <- mean( abs( ytest * log(ytest/mod$est) ), na.rm = TRUE)
     }
   }
-  
-  if ( B > 1 ) {
-  
-    for ( j in nt ) {
-      for ( i in 2:nfolds ) {
-        est[[ i ]][[ j ]] <- rbind( est[[ i - 1 ]][[ j ]], est[[ i ]][[ j ]] )
-      }
-    }
 
-    for (j in nt)   est[[ j ]] <- est[[ nfolds ]][[ j ]] ## compositional predictions
-    if ( max(nt) < nfolds )  for (j in nfolds:c(max(nt) + 1) )  est[[ j ]] <- NULL
-    ina <- unlist(folds)
-    yy <- y[ina, ]
-  
-    perf <- numeric(B)
-    for (i in 1:B) {
-      ind <- sample(n, n, replace = TRUE)
-      a <- numeric( length(nt) )
-      for ( j in 1:length(nt) )  a[j] <- mean( abs( yy[ind, ] * log(yy[ind, ]/est[[ j ]][ind, ]) ), na.rm = TRUE)
-      m <- which.min(a)
-      perf[i] <- mean( abs( yy[-ind, ] * log(yy[-ind, ]/est[[ m ]][-ind, ]) ), na.rm = TRUE)
-    }
-	res <- list( kl = 2 * kl, bc.perf = 2 * mean(perf) )
-  }  else 	res <- list( kl = 2 * kl, bc.perf = 2 * kl )
-  
+ 	res <- list( kl = 2 * kl )
+
   runtime <- proc.time() - runtime
   res$runtime <- runtime
-  res  
+  res
 }
--- R/lc.reg.R
+++ R/lc.reg.R
@@ -3,20 +3,22 @@ lc.reg <- function(y, x, xnew = NULL) {
  x <- log(x)
  dm <- dim(x)
  n <- dm[1]  ;  p <- dm[2]
- R <- rep(1, p)   ;   ca <- 0
+ x <- cbind(1, x)
+ R <- c(0, rep(1, p) )  ;   ca <- 0
  xxs <- solve( crossprod(x) )
  bols <- xxs %*% crossprod(x, y)
  com <- xxs %*% R %*% solve( R %*% xxs %*% R)
- be <- bols - as.vector( com %*% R %*% bols ) - ca 
+ be <- as.vector( bols - com %*% R %*% bols ) - ca
  e <- y - x %*% be
  va <- sum(e^2) / (n - p + 1)
  covbe <- ( xxs - com %*% R %*% xxs ) * va
  nama <- colnames(x)
- if ( is.null(nama) )  nama <- paste("X", 1:p, sep = "")
+ if ( is.null(nama) )  nama <- c( "constant", paste("X", 1:p, sep = "") )
+ if ( nama[1] == "" )  nama[1] <- "constant"
  names(be) <- nama
  colnames(covbe) <- rownames(covbe) <- nama
  est <- NULL
- if ( !is.null(xnew) )  est <- log(xnew) %*% be
+ if ( !is.null(xnew) )  est <- cbind(1, log(xnew) ) %*% be
 
  list(be = be, covbe = covbe, va = va, residuals = e, est = est)
 }
